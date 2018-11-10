98a74a7a31ee3c9c8c5276cc83e0119d20c73103
liushao <liushao@meizhitec.com>
2018-11-10 17:52:10 +0800

    更改melpa源

--- init.el
+++ init.el
@@ -11,8 +11,6 @@
   (message "Your Emacs is old, and some functionality in this config will be disabled. Please upgrade if possible."))
 
 (add-to-list 'load-path (expand-file-name "lisp" user-emacs-directory))
-(add-to-list 'package-archives '("gun" . "http://elpa.emacs-china.org/gun/"))
-(add-to-list 'package-archives '("melpa" . "http://elpa.emacs-china.org/melpa/"))
 (require 'init-benchmarking) ;; Measure startup time
 
 (defconst *spell-check-support-enabled* nil) ;; Enable with t if you prefer
